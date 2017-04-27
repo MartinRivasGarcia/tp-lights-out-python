d99246b3622a62f0d1415126f80a5c36dd435b73
MartinRivas <Martin@RivasGarcia.com.ar>
2017-04-27 17:59:54 -0300

    Se arreglo la funcion de interaccion con el usuario de forma tal de poder volver a pedir las coordenas, en caso de que estas no esten en el rango pedido

--- niveles.py
+++ niveles.py
@@ -1,4 +1,4 @@
-def Generacion(nivel):
+def generacion(nivel):
     if (nivel == 1):
         lista = [
             '0 0 . 0 0',
@@ -41,36 +41,50 @@ def Generacion(nivel):
         ]
     return (lista)
 
-def Imprimir (nivel,grilla):
+def imprimir (nivel,grilla):
     print("Usted esta jugando el nivel " + str(nivel) + ":")
     print("   A B C D E ")
     numero = 1
     for renglon in grilla:
         print(str(numero) + " |" + renglon)
         numero = numero + 1
+    print()
 
-def UsuarioJugando (tamaño):
+def usuario_jugando (tamaño):
+
     a = input("Ingrese las coordenadas a utilizar: ")
-    if ((int(a[0]) > tamaño) or (int(a[0]) <= 0)):
-        print("El numero de las filas ingresado es invalido")
+    if(a[0] < "A"):
+        if ((int(a[0]) > tamaño) or (int(a[0]) <= 0)):
+            print("El numero de las filas ingresado es invalido")
+            error = 1
+        else:
+            error=0
+    else:
+        print("La primer coordenada debe ser un numero referido a las filas")
         error = 1
+
     if ((a[1] < "a" or a[1] > "e") and (a[1] > "A" or a[1] < "E")):
         print("La letra referida a las columnas ingresada es invalida")
         error = 1
     while(error == 1):
         a = input("Reingrese las coordenadas a utilizar: ")
-        if ((int(a[0]) > tamaño) or (int(a[0]) <= 0)):
-            print("El numero de las filas ingresado es invalido")
-            error = 1
+        if (a[0] < "A"):
+            if ((int(a[0]) > tamaño) or (int(a[0]) <= 0)):
+                print("El numero de las filas ingresado es invalido")
+                error = 1
+            else:
+                error = 0
         else:
-            error = 0
+            print("La primer coordenada debe ser un numero referido a las filas")
+            error = 1
+
         if ((a[1] < "a" or a[1] > "e") and (a[1] > "A" or a[1] < "E")):
             print("La letra referida a las columnas ingresada es invalida")
             error = 1
-    print(a)
+    return (a)
 
 #Tener en cuenta para el modo de juego alternativo
-def GenerarRandom():
+def generar_random():
     import random
     caracter = random.random() #Devuelve entre 0.0 y 1.0
     if (caracter > 0.5):
