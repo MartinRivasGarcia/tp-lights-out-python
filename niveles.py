7ef5cb9d294241ef804aaa418ae79ba4721459fc
Maq301 <Martin@RivasGarcia.com.ar>
2017-05-02 18:27:09 -0300

    Se cambio la estructura de datos de las grillad de listas a diccionarios con listas, de forma tal que sea mas manipulable. Se corrigio la parte del programa cuando el usuario elige reiniciar, debido a que una vez reiniciado no se podia continuar.

--- niveles.py
+++ niveles.py
@@ -1,62 +1,65 @@
 #Genera las grillas de los niveles predeterminados
-def generacion(nivel):
+def generacion_grilla(nivel):
     #La funcion unicamente te crea la grilla si la variable de entrada esta entre 1 y 5
     if (nivel == 1):
-        lista = [
-            '0 0 . 0 0',
-            '0 . 0 . 0',
-            '. 0 0 0 .',
-            '0 . 0 . 0',
-            '0 0 . 0 0',
-        ]
+        lista = {
+            '1':['0','0','.','0','0'],
+            '2':['0','.','0','.','0'],
+            '3':['.','0','0','0','.'],
+            '4':['0','.','0','.','0'],
+            '5':['0','0','.','0','0']
+        }
     if (nivel == 2):
-        lista = [
-            '. 0 . 0 .',
-            '0 0 . 0 0',
-            '. 0 . 0 .',
-            '0 . 0 . 0',
-            '0 . 0 . 0',
-        ]
+        lista = {
+            '1':['.','0','.','0','.'],
+            '2':['0','0','.','0','0'],
+            '3':['.','0','.','0','.'],
+            '4':['0','.','0','.','0'],
+            '5':['0','.','0','.','0']
+        }
     if (nivel == 3):
-        lista = [
-            '0 . . . 0',
-            '0 0 . 0 0',
-            '. . 0 . .',
-            '0 . 0 . .',
-            '0 . 0 0 .',
-        ]
+        lista = {
+            '1':['0','.','.','.','0'],
+            '2':['0','0','.','0','0'],
+            '3':['.','.','0','.','.'],
+            '4':['0','.','0','.','.'],
+            '5':['0','.','0','0','.']
+        }
     if (nivel == 4):
-        lista = [
-            '0 0 . 0 0',
-            '. . . . .',
-            '0 0 . 0 0',
-            '. . . . 0',
-            '0 0 . . .',
-        ]
+        lista = {
+            '1':['0','0','.','0','0'],
+            '2':['.','.','.','.','.'],
+            '3':['0','0','.','0','0'],
+            '4':['.','.','.','.','0'],
+            '5':['0','0','.','.','.']
+        }
     if (nivel == 5):
-        lista = [
-            '. . . 0 0',
-            '. . . 0 0',
-            '. . . . .',
-            '0 0 . . .',
-            '0 0 . . .',
-        ]
+        lista = {
+            '1':['.','.','.','0','0'],
+            '2':['.','.','.','0','0'],
+            '3':['.','.','.','.','.'],
+            '4':['0','0','.','.','.'],
+            '5':['0','0','.','.','.']
+        }
     #va a devolver una lista creada para ser la grilla del nivel sin afectar la variable de entrada
     return (lista)
 
 #Imprime en pantalla la grilla del nivel inicial, la cual ira siendo modificada
-def imprimir (nivel,grilla):
+def imprimir_grilla (nivel,grilla,tamaño):
     print("Usted esta jugando el nivel " + str(nivel) + ":")
     print("    A B C D E ")
-    numero = 1
-    for renglon in grilla:
-        print(str(numero) + " | " + renglon)
-        numero = numero + 1
+    for numero in range(1,(tamaño+1)):
+        posicionG = str(numero)
+        print(str(numero) + " | ",end="")
+        for elemento in grilla[posicionG]:
+            print(elemento, end=" ")
+        print()
     print()
 
 #Devuelve las coordenadas que el usuario ingreso
 def coordenadas_ingresadas (tamaño):
     a = input("Ingrese las coordenadas a utilizar: ")
+    final = limite(tamaño)
     if(a[0] < "A"):
         if ((int(a[0]) > tamaño) or (int(a[0]) <= 0)):
             print("El numero de las filas ingresado es invalido")
@@ -67,7 +70,7 @@ def coordenadas_ingresadas (tamaño):
         print("La primer coordenada debe ser un numero referido a las filas")
         error = 1
 
-    if ((a[1] < "a" or a[1] > "e") and (a[1] > "A" or a[1] < "E")):
+    if ((a[1] < "a" or a[1] > final[0]) and (a[1] < "A" or a[1] > final[1])):
         print("La letra referida a las columnas ingresada es invalida")
         error = 1
     while(error == 1):
@@ -82,11 +85,25 @@ def coordenadas_ingresadas (tamaño):
             print("La primer coordenada debe ser un numero referido a las filas")
             error = 1
 
-        if ((a[1] < "a" or a[1] > "e") and (a[1] > "A" or a[1] < "E")):
+        if ((a[1] < "a" or a[1] > final[0]) and (a[1] < "A" or a[1] > final[1])):
             print("La letra referida a las columnas ingresada es invalida")
             error = 1
     print()
     return (a)
+
+def limite(tamaño):
+    if(tamaño == 5):
+        return("e","E")
+    if(tamaño == 6):
+        return("f","F")
+    if(tamaño == 7):
+        return("g","G")
+    if(tamaño == 8):
+        return("h","H")
+    if(tamaño == 9):
+        return("i","I")
+    if(tamaño == 10):
+        return("j","J")
 
 #Tener en cuenta para el modo de juego alternativo
 def generar_random():
@@ -99,22 +116,23 @@ def generar_random():
     return(caracter)
 
 #Tener en cuenta para cuando el usuario reinicie el juego
-def puntaje_por_encendidas(puntaje,grilla):
-    for linea in grilla:
-        for caracter in linea:
-            if(caracter == "0"):
-                if(puntaje > 0):
-                    puntaje = puntaje-50
+def puntaje_por_encendidas(puntaje,grilla,tamaño):
+    for numero in range(1,(tamaño+1)):
+        posicion=str(numero)
+        for caracter in grilla[posicion]:
+            if((caracter == "0") and (puntaje > 0)):
+                puntaje = puntaje-50
     return (puntaje)
+
 def usuario_jugando(tamaño):
     print("¿Que desea hacer a continuacion?")
-    print("0 - Ingrasar coordenadas para jugar")
+    print("0 - Ingresar coordenadas para jugar")
     print("1 - Reiniciar el nivel y restar 50  puntos por cada luz encendida")
     print("2 - Salir del juego")
     siguio = input("Opcion elegida: ")
     if((siguio != "0") and (siguio != "1") and (siguio != "2")):
         print("La opcion elegida no es valida, recuerde: ")
-        print("0 - Ingrasar coordenadas para jugar")
+        print("0 - Ingresar coordenadas para jugar")
         print("1 - Reiniciar el nivel y restar 50  puntos por cada luz encendida")
         print("2 - Salir del juego")
         siguio = input("Opcion elegida: ")
@@ -122,6 +140,3 @@ def usuario_jugando(tamaño):
         return (coordenadas_ingresadas(tamaño))
     else:
         return (siguio)
-"""def usuario_jugo(coordenadas):
-    coordenadas = lower(coordenadas)
-    coordenadas [1] = ord(coordenadas [1]) - 97"""
--- principal.py
+++ principal.py
@@ -6,7 +6,7 @@ juego,tamaño = menu.modos_de_juego(0)
 if((juego == "0") and (tamaño == 0)):
     juego,tamaño = menu.instrucciones()
 
-if(juego=="3" or juego=="2"):
+if(juego == "3" or juego == "2"):
         print("Gracias por jugar a lights out")
         print("Vuelva pronto")
         exit()
@@ -14,16 +14,19 @@ nivel = 1
 ganar = 0
 reiniciar = 0
 puntaje = 0
+
 if (juego == "1"):
     while(True):
-        grilla = niveles.generacion(nivel)
+        grilla = niveles.generacion_grilla(nivel)
         while (ganar != 1):
-            niveles.imprimir(nivel,grilla)
+            niveles.imprimir_grilla(nivel,grilla,tamaño)
             coordenadas = niveles.usuario_jugando(tamaño) #Falta definir que hago si elige reiniciar, tambien falta ver como juego
-            if(coordenadas == "2"):
+            if(coordenadas == "2"): #Elijio salir
                 print("Gracias por jugar a lights out")
                 print("Vuelva pronto")
                 exit()
-            if(coordenadas == "1"):
-                grilla = niveles.usuario_jugo(coordenadas)
-    nivel = nivel + 1
+            elif(coordenadas == "1"): #Reinicio
+                puntaje = niveles.puntaje_por_encendidas(puntaje,grilla,tamaño)
+                print(puntaje)
+            #else: #Jugo
+        nivel = nivel + 1
